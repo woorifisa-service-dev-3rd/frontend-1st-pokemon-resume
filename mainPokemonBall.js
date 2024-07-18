46c186ab8e962ddffd0c13ea29ab00ef1094ee40
YE-EUN <musiclove529@naver.com>
2024-07-19 04:07:10 +0900

    fix: 데이터 불규칙적으로 적용되는 문제 해결
    iframe(modal) 부분이 완전히 로드된 후에 요소를 선택해야 하는데 로드되기
    전에 이벤트 리스너가 호출되버려서 요소들이 null값을 가지기 때문에 데이터
    적용이 안 됨 -> DOMContentLoaded 이벤트 리스너를 사용하여 문제 해결
    
    issue: #39

--- mainPokemonBall.js
+++ mainPokemonBall.js
@@ -5,79 +5,53 @@ const img_monsterball_2 = document.getElementById("img_monsterball_2");
 const img_monsterball_3 = document.getElementById("img_monsterball_3");
 const img_monsterball_4 = document.getElementById("img_monsterball_4");
 
-const modal_profile_img_tech_1 = document
-  .getElementById("modal")
-  .contentWindow.document.getElementById("img_tech_1");
-const modal_profile_img_tech_2 = document
-  .getElementById("modal")
-  .contentWindow.document.getElementById("img_tech_2");
-const modal_profile_img_tech_3 = document
-  .getElementById("modal")
-  .contentWindow.document.getElementById("img_tech_3");
-const modal_profile_img_tech_4 = document
-  .getElementById("modal")
-  .contentWindow.document.getElementById("img_tech_4");
+document.getElementById("modal").addEventListener("load", () => {
+  const modalDocument = document.getElementById("modal").contentWindow.document;
+  
+  const modal_profile_img_tech_1 = modalDocument.getElementById("img_tech_1");
+  const modal_profile_img_tech_2 = modalDocument.getElementById("img_tech_2");
+  const modal_profile_img_tech_3 = modalDocument.getElementById("img_tech_3");
+  const modal_profile_img_tech_4 = modalDocument.getElementById("img_tech_4");
 
-// iframe속 변수
-const modal_profile_name = document
-  .getElementById("modal")
-  .contentWindow.document.getElementById("profile_name");
+  const modal_profile_name = modalDocument.getElementById("profile_name");
+  const modal_profile_img = modalDocument.getElementById("profile_img");
+  const modal_profile_appeard_area_1 = modalDocument.getElementById("appeared_area_1");
+  const modal_profile_appeard_area_2 = modalDocument.getElementById("appeared_area_2");
+  const modal_profile_hobby = modalDocument.getElementById("profile_hoddy");
+  const modal_profile_hours = modalDocument.getElementById("profile_hours");
+  const modal_profile_mbti = modalDocument.getElementById("profile_mbti");
 
-const modal_profile_img = document
-  .getElementById("modal")
-  .contentWindow.document.getElementById("profile_img");
+  img_monsterball_1.addEventListener("mouseover", () => {
+    setInfo(hong.data);
+    console.log(hong.data.name);
+  });
+  img_monsterball_2.addEventListener("mouseover", () => {
+    setInfo(jo.data);
+    console.log(jo.data.name);
+  });
+  img_monsterball_3.addEventListener("mouseover", () => {
+    setInfo(choi.data);
+    console.log(choi.data.name);
+  });
+  img_monsterball_4.addEventListener("mouseover", () => {
+    setInfo(lee.data);
+    console.log(lee.data.name);
+  });
 
-const modal_profile_appeard_area_1 = document
-  .getElementById("modal")
-  .contentWindow.document.getElementById("appeared_area_1");
+  function setInfo(personInfo) {
+    modal_profile_name.textContent = `${personInfo.name} ${personInfo.github}`;
+    modal_profile_img.src = personInfo.profile_img;
+    modal_profile_hobby.textContent = personInfo.hobby;
+    modal_profile_hours.textContent = personInfo.hours;
+    modal_profile_mbti.textContent = personInfo.mbti;
+    modal_profile_img_tech_1.src = personInfo.tech_stack[0];
+    modal_profile_img_tech_2.src = personInfo.tech_stack[1];
+    modal_profile_img_tech_3.src = personInfo.tech_stack[2];
+    modal_profile_img_tech_4.src = personInfo.tech_stack[3];
 
-const modal_profile_appeard_area_2 = document
-  .getElementById("modal")
-  .contentWindow.document.getElementById("appeared_area_2");
-
-const modal_profile_hobby = document
-  .getElementById("modal")
-  .contentWindow.document.getElementById("profile_hoddy");
-
-const modal_profile_hours = document
-  .getElementById("modal")
-  .contentWindow.document.getElementById("profile_hours");
-
-const modal_profile_mbti = document
-  .getElementById("modal")
-  .contentWindow.document.getElementById("profile_mbti");
-
-img_monsterball_1.addEventListener("mouseover", () => {
-  setInfo(hong.data);
-  console.log("여기자나" + hong.data.hobby);
+    modal_profile_appeard_area_1.style.backgroundColor = personInfo.stations[0].station_color;
+    modal_profile_appeard_area_1.textContent = personInfo.stations[0].name;
+    modal_profile_appeard_area_2.style.backgroundColor = personInfo.stations[1].station_color;
+    modal_profile_appeard_area_2.textContent = personInfo.stations[1].name;
+  }
 });
-img_monsterball_2.addEventListener("mouseover", () => {
-  setInfo(jo.data);
-});
-img_monsterball_3.addEventListener("mouseover", () => {
-  setInfo(choi.data);
-});
-img_monsterball_4.addEventListener("mouseover", () => {
-  setInfo(lee.data);
-});
-
-function setInfo(personInfo) {
-  modal_profile_name.textContent = `${personInfo.name} ${personInfo.github}`;
-  modal_profile_img.src = personInfo.profile_img;
-  // modal_profile_station = personInfo.stations;
-  modal_profile_hobby.textContent = personInfo.hobby;
-  modal_profile_hours.textContent = personInfo.hours;
-  modal_profile_mbti.textContent = personInfo.mbti;
-  modal_profile_img_tech_1.src = personInfo.tech_stack[0];
-  modal_profile_img_tech_2.src = personInfo.tech_stack[1];
-  modal_profile_img_tech_3.src = personInfo.tech_stack[2];
-  modal_profile_img_tech_4.src = personInfo.tech_stack[3];
-  //색상, 텍스트 넣기
-  modal_profile_appeard_area_1.style.backgroundColor =
-    personInfo.stations[0].station_color;
-  modal_profile_appeard_area_1.textContent = personInfo.stations[0].name;
-
-  modal_profile_appeard_area_2.style.backgroundColor =
-    personInfo.stations[1].station_color;
-  modal_profile_appeard_area_2.textContent = personInfo.stations[1].name;
-}
