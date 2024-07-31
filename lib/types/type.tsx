9d5a301514242bcb3f73a21ad2962f44400e7578
YE-EUN <musiclove529@naver.com>
2024-07-31 16:34:01 +0900

    feat: firestore 연동 및 기본 쿼리 생성
    issue: #44

--- lib/types/type.tsx
+++ lib/types/type.tsx
@@ -1,23 +1,32 @@
 // 프로젝트 희망 기간 변수
-export type DueDate = '1month' | '2month' | '3month' |
-                      '4month' | '5month' | '6month' | 
-                      '7month' | '8month' | '9month' | 
-                      '10month' | '11month' | '12month';
+export type DueDate =
+  | "1month"
+  | "2month"
+  | "3month"
+  | "4month"
+  | "5month"
+  | "6month"
+  | "7month"
+  | "8month"
+  | "9month"
+  | "10month"
+  | "11month"
+  | "12month";
 
 // User 테이블 구조
 export interface User {
-  id: string;			// firebase 해시 값
-	email: string;
+  id: string; // firebase 해시 값
+  email: string;
   password: string;
   user_info: UserInfo;
-	team: User[];
+  team: User[];
 }
 
 // UserInfo 테이블 구조
 export interface UserInfo {
   name: string;
   age: number;
-	phone: string;
+  phone: string;
   skill_stack: Skill[];
   profile_img: string;
   github_id: string;
@@ -28,4 +37,3 @@ export interface Skill {
   name: string;
   img_url: string;
 }
-
