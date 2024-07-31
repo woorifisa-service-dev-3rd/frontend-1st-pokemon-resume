a0cbb24078a9c0c63fdce8dc9fdcda8cd74e4fe5
YE-EUN <musiclove529@naver.com>
2024-08-01 01:12:26 +0900

    feat: firebase 인증 적용
    추가적으로 한 일
    - add할 때 addDoc() -> setDoc()
    - DocType 생성
    - 주석 작성
    
    issue: #52

--- lib/utils/auth.ts
+++ lib/utils/auth.ts
@@ -1,4 +1,43 @@
 import { app } from "./firebaseApp";
-import { getAuth } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { add, getById } from "@/lib/utils/db";
+import { User } from "@/lib/types/type";
 
-export const auth = getAuth(app);
+const auth = getAuth(app);
+// const email = "이메일 작성";
+// const password = "비밀번호 6자리 이상 작성";
+
+// 회원가입
+export const signIn = (email: string, password: string, data: User) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user.uid, user.email);
+
+      data.id = user.uid;
+      data.email = String(user.email);
+      console.log(data.id, data.email);
+
+      add("users", user.uid, data);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    });
+};
+
+// 로그인
+export const logIn = (email: string, password: string) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      // TODO: 로그인 후 처리 로직
+      getById("users", user.uid);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    });
+};
--- lib/utils/db.ts
+++ lib/utils/db.ts
@@ -1,28 +1,23 @@
+import { DocType } from "../types/type";
 import { app } from "./firebaseApp";
 
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-  doc,
-  setDoc,
-  query,
-  where,
-} from "firebase/firestore";
+import { getFirestore, collection, doc, setDoc, getDocs, query, where } from "firebase/firestore";
 
-// Initialize Cloud Firestore and get a reference to the service
-export const db = getFirestore(app);
+// Firestore 초기화
+const db = getFirestore(app);
 
-export const add = async (col: string, data: Object) => {
+// collection에 document 추가
+export const add = async (col: string, id: string, data: DocType) => {
   try {
-    const docRef = await addDoc(collection(db, col), data);
+    const docRef = doc(db, col, id);
+    await setDoc(docRef, data);
     console.log("Document written with ID: ", docRef.id);
   } catch (e) {
     console.error("Error adding document: ", e);
   }
 };
 
+// collection 전체 document 조회
 export const get = async (col: string) => {
   const querySnapshot = await getDocs(collection(db, col));
   querySnapshot.forEach((doc) => {
@@ -30,15 +25,16 @@ export const get = async (col: string) => {
   });
 };
 
-export const getById = async (col: string, email: string) => {
-  const q = query(collection(db, col), where("email", "==", email));
+// collection에서 id 값으로 document 조회
+export const getById = async (col: string, id: string) => {
+  const q = query(collection(db, col), where("id", "==", id));
   const querySnapshot = await getDocs(q);
   querySnapshot.forEach((doc) => {
-    // doc.data() is never undefined for query doc snapshots
     console.log(doc.id, " => ", doc.data());
   });
 };
 
-export const set = async (col: string, docId: string, data: Object) => {
-  await setDoc(doc(db, col, docId), data);
+// collection에서 특정 document 수정
+export const set = async (col: string, id: string, data: DocType) => {
+  await setDoc(doc(db, col, id), data);
 };
