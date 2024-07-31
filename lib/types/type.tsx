4687c7d7a3a456896a10f9576b35238d5f5f76f3
ChanUi <hcu55@naver.com>
2024-07-31 12:58:24 +0900

    feat : 테이블 타입 수정
    issue : #42

--- lib/types/type.tsx
+++ lib/types/type.tsx
@@ -1,25 +1,31 @@
-export interface Users {
-  id: string;
-  password: string;
-  phone: string;
-  user_info: UserInfo;
-}
-
+// 프로젝트 희망 기간 변수
 export type DueDate = '1month' | '2month' | '3month' |
                       '4month' | '5month' | '6month' | 
                       '7month' | '8month' | '9month' | 
                       '10month' | '11month' | '12month';
 
+// User 테이블 구조
+export interface User {
+  id: string;			// firebase 해시 값
+	email: string;
+  password: string;
+  user_info: UserInfo;
+	team: User[];
+}
+
+// UserInfo 테이블 구조
 export interface UserInfo {
   name: string;
   age: number;
-  email: string;
+	phone: string;
   skill_stack: Skill[];
   profile_img: string;
   github_id: string;
   project_due: DueDate;
 }
+
 export interface Skill {
   name: string;
   img_url: string;
 }
+
