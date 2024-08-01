7b7c2046d13f662c99bafe942452a6dcc3a93969
kh-0818 <lkh3179@naver.com>
2024-08-01 12:11:10 +0900

    feat:비밀번호 재설정 삭제
     삭.제.

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -18,21 +18,7 @@ const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
 };
 
 const App: React.FC = () => {
-  const [isResetPasswordModalOpen, setIsResetPasswordModalOpen] =
-    useState(false);
   const [isSignUpModalOpen, setIsSignUpModalOpen] = useState(false);
-
-  const showResetPasswordModal = () => {
-    setIsResetPasswordModalOpen(true);
-  };
-
-  const handleResetPasswordOk = () => {
-    setIsResetPasswordModalOpen(false);
-  };
-
-  const handleResetPasswordCancel = () => {
-    setIsResetPasswordModalOpen(false);
-  };
 
   const showSignUpModal = () => {
     setIsSignUpModalOpen(true);
@@ -46,167 +32,133 @@ const App: React.FC = () => {
     setIsSignUpModalOpen(false);
   };
   return (
-    <div className="flex">
-      <h1>회원가입</h1>
-
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-        layout={"vertical"}
-      >
-        <Form.Item<FieldType>
-          label="ID(이메일)"
-          name="username"
-          rules={[{ required: true, message: "Please input your username!" }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-          name="remember"
-          valuePropName="checked"
-          wrapperCol={{ span: 16 }}
-        >
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item wrapperCol={{ span: 16 }}>
-          <div style={{ display: "flex", gap: "10px" }}>
-            <Button type="primary" htmlType="submit">
-              로그인
-            </Button>
-            <Button type="primary" htmlType="button" onClick={showSignUpModal}>
-              회원가입
-            </Button>
-            <Button type="primary" onClick={showResetPasswordModal}>
-              비밀번호 재설정
-            </Button>
-          </div>
-        </Form.Item>
-      </Form>
-      <Modal
-        open={isResetPasswordModalOpen}
-        onOk={handleResetPasswordOk}
-        onCancel={handleResetPasswordCancel}
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
       >
         <Form
-          name="reset_password"
+          name="basic"
           labelCol={{ span: 8 }}
           wrapperCol={{ span: 16 }}
-          style={{ maxWidth: 600 }}
-          onFinish={(values) => console.log("Password Reset Success:", values)}
-          onFinishFailed={(errorInfo) =>
-            console.log("Password Reset Failed:", errorInfo)
-          }
+          style={{
+            width: "fit-content",
+          }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+          layout={"vertical"}
         >
+          <h1 style={{ textAlign: "center" }}>로그인</h1>
+          <br />
           <Form.Item<FieldType>
-            label="새 비밀번호"
-            name="newPassword"
-            rules={[
-              {
-                required: true,
-                message: "6자 이상의 새 비밀번호를 입력하시오",
-                min: 6,
-              },
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-          <Form.Item<FieldType>
-            label="새 비밀번호 확인"
-            name="confirmNewPassword"
-            dependencies={["newPassword"]}
-            rules={[
-              {
-                required: true,
-                message: "새 비밀번호를 확인하시오",
-              },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue("newPassword") === value) {
-                    return Promise.resolve();
-                  }
-                  return Promise.reject(
-                    new Error("비밀번호가 일치하지 않습니다.")
-                  );
-                },
-              }),
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-        </Form>
-      </Modal>
-
-      <Modal
-        open={isSignUpModalOpen}
-        onOk={handleSignUpOk}
-        onCancel={handleSignUpCancel}
-      >
-        <Form
-          name="sign_up"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          style={{ maxWidth: 600 }}
-          onFinish={(values) => console.log("Sign Up Success:", values)}
-          onFinishFailed={(errorInfo) =>
-            console.log("Sign Up Failed:", errorInfo)
-          }
-        >
-          <Form.Item<FieldType>
-            label="ID(이메일)"
+            label="Id"
             name="username"
             rules={[{ required: true, message: "Please input your username!" }]}
           >
-            <Input />
+            <Input
+              style={{ width: 400 }}
+              placeholder="이메일 형식으로 입력하시오."
+            />
           </Form.Item>
+
           <Form.Item<FieldType>
             label="Password"
             name="password"
             rules={[{ required: true, message: "Please input your password!" }]}
           >
-            <Input.Password placeholder="6자 이상" />
+            <Input.Password style={{ width: 400 }} />
           </Form.Item>
+
           <Form.Item<FieldType>
-            label="Password 확인"
-            name="confirmPassword"
-            dependencies={["password"]}
-            rules={[
-              {
-                required: true,
-                message: "비밀번호를 확인하시오",
-              },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue("password") === value) {
-                    return Promise.resolve();
-                  }
-                  return Promise.reject(
-                    new Error("비밀번호가 일치하지 않습니다.")
-                  );
-                },
-              }),
-            ]}
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{ span: 16 }}
           >
-            <Input.Password />
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ span: 16 }}>
+            <div style={{ display: "flex", gap: "10px" }}>
+              <Button type="primary" htmlType="submit">
+                로그인
+              </Button>
+              <Button
+                type="primary"
+                htmlType="button"
+                onClick={showSignUpModal}
+              >
+                회원가입
+              </Button>
+            </div>
           </Form.Item>
         </Form>
-      </Modal>
-    </div>
+
+        <Modal
+          open={isSignUpModalOpen}
+          onOk={handleSignUpOk}
+          onCancel={handleSignUpCancel}
+        >
+          <Form
+            name="sign_up"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600 }}
+            onFinish={(values) => console.log("Sign Up Success:", values)}
+            onFinishFailed={(errorInfo) =>
+              console.log("Sign Up Failed:", errorInfo)
+            }
+          >
+            <Form.Item<FieldType>
+              label="ID(이메일)"
+              name="username"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Input autoComplete="off" />
+            </Form.Item>
+            <Form.Item<FieldType>
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input.Password placeholder="6자 이상" />
+            </Form.Item>
+            <Form.Item<FieldType>
+              label="Password 확인"
+              name="confirmPassword"
+              dependencies={["password"]}
+              rules={[
+                {
+                  required: true,
+                  message: "비밀번호를 확인하시오",
+                },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue("password") === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      new Error("비밀번호가 일치하지 않습니다.")
+                    );
+                  },
+                }),
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+          </Form>
+        </Modal>
+      </div>
+    </>
   );
 };
 
